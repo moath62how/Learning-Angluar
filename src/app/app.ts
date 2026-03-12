f42bab7439240eb7cce2b8d2bdc92dfe3943b189
moath62how <moath26how@gmail.com>
2026-03-12 06:34:56 +0200

    Trying componets creation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Rooms } from "./rooms/rooms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  // template: `<h1>Hello from Angular! Testing build time</h1>
-  //   <p>This is using multi line</p> `,
   styleUrl: './app.scss',
+  imports:[Rooms]
 })
 export class App {
   protected readonly title = signal('newapp');
