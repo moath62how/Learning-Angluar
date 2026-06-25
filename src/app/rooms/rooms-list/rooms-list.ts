34ba29171f72ea1a5c5ecac638590831f2451d14
moath62how <moath26how@outlook.com>
2026-06-25 19:43:25 +0300

    tes

---
+++ src/app/rooms/rooms-list/rooms-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms-list',
+  imports: [],
+  templateUrl: './rooms-list.html',
+  styleUrl: './rooms-list.scss',
+})
+export class RoomsList {}
--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IRoom } from './IRooms';
 import { IRoomList } from './IRoomList';
 import { DatePipe, CurrencyPipe, TitleCasePipe, SlicePipe } from '@angular/common';
@@ -8,46 +8,56 @@ import { DatePipe, CurrencyPipe, TitleCasePipe, SlicePipe } from '@angular/commo
   styleUrl: './rooms.scss',
   imports: [DatePipe, TitleCasePipe, CurrencyPipe, SlicePipe],
 })
-export class Rooms {
+export class Rooms implements OnInit {
   hotelName: string = 'Hilton';
-  numberOfRooms: Number = 10;
-  hideRooms: boolean = false;
-  toggle() {
-    this.hideRooms = !this.hideRooms;
-  }
   rooms: IRoom = {
     availableRooms: 5,
     bookedRooms: 5,
     totalRooms: 15,
   };
+  hideRooms: boolean = false;
+  roomsList: IRoomList[] = [];
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
 
-  roomlist: IRoomList[] = [
-    {
-      roomNumber: 101,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioning, Free Wi-Fi, Flat-screen TV',
-      price: 150,
-      photos: 'https://imges.unsplash.com/photo-1560448070-8bfa1e9cbd9b',
-      checkinTime: new Date('2024-07-01T14:00:00'),
-      checkoutTime: new Date('2024-07-02T12:00:00'),
-    },
-    {
-      roomNumber: 102,
-      roomType: 'Standard Room',
-      amenities: 'Air Conditioning, Free Wi-Fi, Flat-screen TV',
-      price: 100,
-      photos: 'https://imges.unsplash.com/photo-1560448070-8bfa1e9cbd9b',
-      checkinTime: new Date('2024-07-01T14:00:00'),
-      checkoutTime: new Date('2024-07-02T12:00:00'),
-    },
-    {
-      roomNumber: 103,
-      roomType: 'Suite Room',
-      amenities: 'Air Conditioning, Free Wi-Fi, Flat-screen TV, Kitchenette',
-      price: 25.5,
-      photos: 'https://imges.unsplash.com/photo-1560448070-8bfa1e9cbd9b',
-      checkinTime: new Date('2024-07-01T14:00:00'),
-      checkoutTime: new Date('2024-07-02T12:00:00'),
-    },
-  ];
+  ngOnInit(): void {
+    this.roomsList = [
+      {
+        roomNumber: 101,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioning, Free Wi-Fi, Flat-screen TV',
+        price: 150,
+        photos: 'https://imges.unsplash.com/photo-1560448070-8bfa1e9cbd9b',
+        checkinTime: new Date('2024-07-01T14:00:00'),
+        checkoutTime: new Date('2024-07-02T12:00:00'),
+      },
+      {
+        roomNumber: 102,
+        roomType: 'Standard Room',
+        amenities: 'Air Conditioning, Free Wi-Fi, Flat-screen TV',
+        price: 100,
+        photos: 'https://imges.unsplash.com/photo-1560448070-8bfa1e9cbd9b',
+        checkinTime: new Date('2024-07-01T14:00:00'),
+        checkoutTime: new Date('2024-07-02T12:00:00'),
+      },
+      {
+        roomNumber: 103,
+        roomType: 'Suite Room',
+        amenities: 'Air Conditioning, Free Wi-Fi, Flat-screen TV, Kitchenette',
+        price: 25.5,
+        photos: 'https://imges.unsplash.com/photo-1560448070-8bfa1e9cbd9b',
+        checkinTime: new Date('2024-07-01T14:00:00'),
+        checkoutTime: new Date('2024-07-02T12:00:00'),
+      },
+    ];
+
+  }
+
+  constructor() {
+
+  }
+
+
+
 }
